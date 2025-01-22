5784aea19b6a3a9733cfcd953c1ec25e668ffd20
Duy\duyuser <fpduypham@gmail.com>
2025-01-22 19:05:14 +0700

    refactor for better

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {ExerciseListComponent} from './exercise/exercise-list/exercise-list.component';
 import {ExerciseCreateComponent} from './exercise/exercise-create/exercise-create.component';
 import {ExerciseDetailComponent} from './exercise/exercise-detail/exercise-detail.component';
@@ -6,6 +6,7 @@ import {ExerciseEditComponent} from './exercise/exercise-edit/exercise-edit.comp
 import {AudioSplitterComponent} from './audio-splitter/audio-splitter.component';
 import {ListenAndWriteComponent} from './listen-and-write/listen-and-write.component';
 import {ExercisePassiveListeningComponent} from './exercise/exercise-passive-listening/exercise-passive-listening.component';
+import {NgModule} from '@angular/core';
 
 // export const routes: Routes = [];
 
@@ -28,15 +29,47 @@ import {ExercisePassiveListeningComponent} from './exercise/exercise-passive-lis
 //     pathMatch: 'full',
 //   }
 // ];
-
+//
+//
+// export const routes: Routes = [
+//   { path: '', redirectTo: '/exercises', pathMatch: 'full' },
+//   { path: 'exercises', component: ExerciseListComponent },
+//   { path: 'exercises/create', component: ExerciseCreateComponent },
+//   { path: 'exercises/:id', component: ExerciseDetailComponent },
+//   { path: 'exercises/:id/edit', component: ExerciseEditComponent },
+//   { path: 'exercises/:id/audio-splitter', component: AudioSplitterComponent },
+//   { path: 'exercises/:id/listen-write', component: ListenAndWriteComponent },
+//   { path: 'exercises/:id/passive-listen', component: ExercisePassiveListeningComponent },
+// ];
 
 export const routes: Routes = [
   { path: '', redirectTo: '/exercises', pathMatch: 'full' },
-  { path: 'exercises', component: ExerciseListComponent },
-  { path: 'exercises/create', component: ExerciseCreateComponent },
-  { path: 'exercises/:id', component: ExerciseDetailComponent },
-  { path: 'exercises/:id/edit', component: ExerciseEditComponent },
-  { path: 'exercises/:id/audio-splitter', component: AudioSplitterComponent },
-  { path: 'exercises/:id/listen-write', component: ListenAndWriteComponent },
-  { path: 'exercises/:id/passive-listen', component: ExercisePassiveListeningComponent },
+  {
+    path: 'exercises',
+    loadComponent: () => import('./exercise/exercise-list/exercise-list.component').then(m => m.ExerciseListComponent)
+  },
+  {
+    path: 'exercises/create',
+    loadComponent: () => import('./exercise/exercise-create/exercise-create.component').then(m => m.ExerciseCreateComponent)
+  },
+  {
+    path: 'exercises/:id',
+    loadComponent: () => import('./exercise/exercise-detail/exercise-detail.component').then(m => m.ExerciseDetailComponent)
+  },
+  {
+    path: 'exercises/:id/edit',
+    loadComponent: () => import('./exercise/exercise-edit/exercise-edit.component').then(m => m.ExerciseEditComponent)
+  },
+  {
+    path: 'exercises/:id/audio-splitter',
+    loadComponent: () => import('./audio-splitter/audio-splitter.component').then(m => m.AudioSplitterComponent)
+  },
+  {
+    path: 'exercises/:id/listen-write',
+    loadComponent: () => import('./listen-and-write/listen-and-write.component').then(m => m.ListenAndWriteComponent)
+  },
+  {
+    path: 'exercises/:id/passive-listen',
+    loadComponent: () => import('./exercise/exercise-passive-listening/exercise-passive-listening.component').then(m => m.ExercisePassiveListeningComponent)
+  },
 ];
